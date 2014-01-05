df21788ad76795812109bfe61b587e043b090aff
legarspol <le-gar_p@epitech.eu>
2014-01-05 09:50:26 +0800

    Add simple analytics in the project

--- src/bugbuster/deviceusage/component/MainActivity.java
+++ src/bugbuster/deviceusage/component/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import com.google.analytics.tracking.android.EasyTracker;
 
 public class MainActivity extends Activity {
 	
@@ -47,4 +48,18 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+	    EasyTracker.getInstance(this).activityStart(this);
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+	    EasyTracker.getInstance(this).activityStop(this);
+	}
+
+	
 }
